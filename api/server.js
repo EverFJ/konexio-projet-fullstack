6e708d2cc0c72ed36a58746ec633bd14f24d8219
Francois Janson <jansonfrancois@gmail.com>
2021-08-05 23:47:06 +0200

    cleaning code

--- api/server.js
+++ api/server.js
@@ -5,22 +5,21 @@ const countries = require("./variables")
 
 const port = 8000;
 
-app.get("/all", cors(), (req, res) => {
+app.use(cors());
+
+app.get("/all", (req, res) => {
     let countriesArray = [];
     for (let i = 0; i < countries.length; i++) {
         countriesArray.push(countries[i].name)
     }
     res.send(countriesArray)
-    // res.send("test")
 })
 
-app.get("/:country", cors(), (req, res) => {
+app.get("/:country", (req, res) => {
     let country = req.params.country;
-    let result = countries.filter(element => element.name == country);
-    console.log(result);
-    result = JSON.stringify(result)
-    
-    res.send(result)
+    let result = countries.filter(element => element.name.toLowerCase() == country.toLowerCase());
+    // console.log(result);
+    res.json(result)
 })
 
 app.listen(port, () => {
--- front/script/script.js
+++ front/script/script.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
     let countries = [];
     const list = $("#countries");
     let listContent = "";
@@ -18,20 +18,23 @@ $(function () {
     }
     getAllCountries()
     $("#btnShowData").click(
-        function () {
+        function() {
             const countryName = $("#inputCountry").val();
             $.ajax({
                 url: 'http://localhost:8000/' + countryName,
-                success: function (data, statuts, response) {
-                    let country = JSON.parse(data)[0];
-                    console.log(country);
-                    listContent = `<li class="list-group-item bg-light">name : ${country.name}</li>
-                    <li class="list-group-item bg-light">capital : ${country.capital}</li>
-                    <li class="list-group-item bg-light">region : ${country.region}</li>
-                    `;
-                    // for (let i = 0; i < country.length; i++) {
-                    //     listContent += `<li class="list-group-item bg-light">${country[i]}</li>`
-                    // }
+                success: function(data, statuts, response) {
+                    console.log(data[0])
+                    const countryProperties = Object.entries(data[0]);
+                    console.log(countryProperties);
+
+                    // listContent = `<li class="list-group-item bg-light">name : ${data[0].name}</li>
+                    // <li class="list-group-item bg-light">capital : ${data[0].capital}</li>
+                    // <li class="list-group-item bg-light">region : ${data[0].region}</li>`;
+
+                    listContent = "";
+                    for (let i = 0; i < countryProperties.length; i++) {
+                        listContent += `<li class="list-group-item bg-light">${countryProperties[i].join(" : ")}</li>`
+                    }
                     list.html(listContent)
                 }
             });
