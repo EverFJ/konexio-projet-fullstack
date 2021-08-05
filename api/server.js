4c086cdefd3e8c49ccfb121360cf876bd1e2b677
Francois Janson <jansonfrancois@gmail.com>
2021-08-05 11:50:32 +0200

    api developement

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const countries = require("./variables")
 
 const port = 8000;
 
+app.get("/all", (req, res) => {
+    let countriesArray = [];
+    for (let i = 0; i < countries.length; i++) {
+        countriesArray.push(countries[i].name)
+    }
+    res.send(countriesArray)
+})
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
-
 })
